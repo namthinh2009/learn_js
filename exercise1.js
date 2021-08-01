b1a3f12b1befabfa523db11d1465e5f365d4642e
namthinh <kimanh02041982@gmail.com>
2021-08-01 21:20:36 +0700

    exercise1

---
+++ exercise1.js
@@ -0,0 +1,12 @@
+var a = 100
+var b = 65
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b )
+console.log(5 % 2)
+var name = 'Nam Thịnh'
+var age = 13
+var grade = 7   
+console.log( name +  ' năm nay ' + age + ' tuổi' + ' và đang học lớp ' + grade )
---
+++ test1.js
@@ -0,0 +1,3 @@
+console.log('xin chào');
+// biến
+// variable
